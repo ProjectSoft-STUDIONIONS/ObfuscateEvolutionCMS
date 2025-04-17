1f2bda9b261e409c51f62121a421af47e3e2feb0
ProjectSoft <projectsoft2009@yandex.ru>
2025-04-17 18:04:44 +0400

    update

--- assets/plugins/obfuscate/plugin.obfuscate.php
+++ assets/plugins/obfuscate/plugin.obfuscate.php
@@ -15,67 +15,17 @@
  * @internal    @disabled 0
  * @author      ProjectSoft <projectsoft2009@yandex.ru>
 */
+use ProjectSoft\Util\Obfuscate;
 
 if(!defined('MODX_BASE_PATH')) die('What are you doing? Get out of here!');
+
+require_once __DIR__ . '/Obfuscate.php';
 
 $modx = EvolutionCMS();
 $e =& $modx->event;
 
-if(!function_exists("ordutf8")):
-	function ordutf8($string, &$offset_obfus) {
-		$code = ord(substr($string, $offset_obfus,1)); 
-		if ($code >= 128):                     //0xxxxxxx
-			if ($code < 224):
-				$bytesnumber = 2;              //110xxxxx
-			else:
-				if ($code < 240):
-					$bytesnumber = 3;          //1110xxxx
-				else:
-					if ($code < 248):
-						$bytesnumber = 4;      //11110xxx
-					endif;
-				endif;
-			endif;
-			$codetemp = $code - 192 - ($bytesnumber > 2 ? 32 : 0) - ($bytesnumber > 3 ? 16 : 0);
-			for ($i = 2; $i <= $bytesnumber; $i++):
-				$offset_obfus ++;
-				$code2 = ord(substr($string, $offset_obfus, 1)) - 128;        //10xxxxxx
-				$codetemp = $codetemp*64 + $code2;
-			endfor;
-			$code = $codetemp;
-		endif;
-		$offset_obfus += 1;
-		if ($offset_obfus >= strlen($string)) $offset_obfus = -1;
-		return $code;
-	}
-endif;
-
-if(!function_exists("obfuscate_replacer")):
-	function obfuscate_replacer(&$matches){
-		$str = trim($matches[2]);
-		$str = preg_replace('|&nbsp;|', ' ',$str);
-		$arr = explode("<br>", $str);
-		$out = array();
-		$offset_obfus = 0;
-		foreach($arr as $key=>$value):
-			$offset_obfus = 0;
-			$obfus = "";
-			while ($offset_obfus >= 0):
-				$obfus .= "&#". ordutf8($value, $offset_obfus) . ";";
-			endwhile;
-			$out[] = $obfus;
-		endforeach;
-		$html = implode("<br>", $out);
-		return $html;
-	}
-endif;
-
 switch ($e->name) {
-	case "OnWebPagePrerender":{
-		$outputPrepare = $modx->documentOutput;
-		$regex = "#(\{obfuscate\}(.+)\{\/obfuscate})#Usi";
-		$outputPrepare = preg_replace_callback($regex, 'obfuscate_replacer', $outputPrepare);
-		$modx->documentOutput = $outputPrepare;
+	case "OnWebPagePrerender":
+		$modx->documentOutput = (new Obfuscate())->render($modx->documentOutput);
 		break;
-	}
 }
