962ddddcf8ce88d5527026b7820da858422f1ccb
ProjectSoft <projectsoft2009@yandex.ru>
2025-04-17 18:06:26 +0400

    update 2.0.0

--- assets/plugins/obfuscate/plugin.obfuscate.php
+++ assets/plugins/obfuscate/plugin.obfuscate.php
@@ -5,7 +5,7 @@
  * obfuscate plugin for Evolution CMS
  *
  * @category    plugin 
- * @version     1.0.0
+ * @version     2.0.0
  * @license     MIT
  * @internal    @properties 
  * @internal    @events OnWebPagePrerender 
